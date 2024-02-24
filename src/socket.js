59cc1507139b645621f8f41fccddf8acdebacde8
Yashgupta9330 <yaahg342@gmail.com>
2024-02-24 20:44:33 +0530

    added url

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,3 @@
-import { io } from 'socket.io-client';
-
-export const socket = io('http://localhost:4000');
-/*export let socketID = '';
-socket.on('connect', () => {
-    socketID = socket.id
-}) */
+import { io } from "socket.io-client";
+const URL = process.env.NODE_ENV === 'production' ? 'https://wrotejs-server.onrender.com' : 'http://localhost:4000'
+export const socket = io(URL);
