22c3f43f9c7dd45d364057aef73a1497f680463b
Yashgupta9330 <yaahg342@gmail.com>
2024-03-06 01:41:27 +0530

    changed in options

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -14,7 +14,7 @@ export default function User({ roomuser }) {
       <Menu as="div" className="relative inline-block text-left">
         <div>
           <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-            Options
+            Users
             <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
           </Menu.Button>
         </div>
